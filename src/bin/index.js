5a8af35d1dad54bb5a05d4c8de1170897eac0c97
Calum Henderson <calumproh28@gmail.com>
2020-07-25 16:04:45 +0100

    R

---
+++ src/bin/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const colortils = require("../console-utils");
+const args = process.argv;
+
+if (args.length) {
+}
--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,5 +1,14 @@
-#!/usr/bin/env node
+const colortils = require('../console-utils');
+// test all consoletils
+const string = colortils.color(
+    'This should be red!',
+    colortils.colors.FgRed,
+);
+const anotherString = 'This should be normal!';
+console.log(string);
+console.log(anotherString);
+const emojiBad = colortils.emoji("Oh no, this should be a cross emoji!", colortils.emojis.Cross);
+const emojiGood = colortils.emoji("This should be good, and a tick emoji!", colortils.emojis.Tick);
+console.log(emojiBad + "\n" + emojiGood);
 
-import consoletils from '../console-utils';
-
-//test all consoletils
+console.log(colortils.color(colortils.emoji(" Test passed!", colortils.emojis.Tick), colortils.colors.FgGreen, colortils.colors.BgBlack, colortils.colors.Bright))
